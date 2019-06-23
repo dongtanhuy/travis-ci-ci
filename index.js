9a673a367630ad570b14174710471dcefabd7c0c
Dong Tan Huy <juliandong@Julians-MacBook-Pro.local>
2019-06-23 15:10:29 +0700

    setup PORT env variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('HELLO')
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('App running on port 3000')
 })
 
